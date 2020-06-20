fcicq/kvm-guest-drivers-windows

commit 1741d661d8f63d2d89a15725773c8eb55371f90b

NetKVM: Implement VIRTIO_CONFIG_S_NEEDS_RESET as needed by spec
The VIRTIO 1.0 spec adds the DEVICE_NEEDS_RESET bit to the device's
status. This commit enables the driver to respods appropriately to
RESET request by the device.

Upon receiving such notification from the device in the control queue we
do the following steps in the following order:

* On Control interrupt read the device status and check that
  VIRTIO_CONFIG_S_NEEDS_RESET bit is set and VIRTIO 1.0 is supported.
* Update the state machine that the device needs a reset, the state
  machine updates all queues using the obervee/oberver logic that they
  can't touch the hardware.
* Lastly the flag bDeviceNeedsReset is set to true. By setting this flag
  to true we inform Windows that the ParaNdis6_Reset should be called by
  NDIS. This happens beacuse in ParaNdis6_CheckForHang we return the value
  of the bDeviceNeedsReset flag which if set instructs Windows to call
  reset function. The ParaNdis6_CheckForHang is called periodically by
  NDIS every few seconds.

Signed-off-by: Sameeh Jubran <sameeh@daynix.com>
Signed-off-by: Basil Salman <basil@daynix.com>

NetKVM/Common/ParaNdis-AbstractPath.h
@@ -110,12 +110,17 @@ public:
         return !This->m_VirtQueue.Restart();
     }
 
-    /* We get notified by the state machine on surprise removal */
+    /* We get notified by the state machine on suprise removal or when the
+       device needs a reset*/
     void Notify(SMNotifications message) override
     {
-        if (message == SupriseRemoved)
+        if (message == SupriseRemoved || message == NeedsReset)
         {
             m_VirtQueue.DoNotTouchHardware();
+        }
+        else if (PoweredOn)
+        {
+            m_VirtQueue.AllowTouchHardware();
         }
     }
 protected:

NetKVM/Common/ParaNdis-Common.cpp
@@ -1880,6 +1880,15 @@ bool ParaNdis_DPCWorkBody(PARANDIS_ADAPTER *pContext, ULONG ulMaxPacketsToIndica
         }
         if (pContext->CXPath.WasInterruptReported())
         {
+            UINT8 status = 0;
+            status = ReadDeviceStatus(pContext);
+            if (virtio_is_feature_enabled(pContext->u64HostFeatures, VIRTIO_F_VERSION_1) &&
+                (status & VIRTIO_CONFIG_S_NEEDS_RESET))
+            {
+                pContext->m_StateMachine.NotifyDeviceNeedsReset();
+                pContext->bDeviceNeedsReset = TRUE;
+            }
+            ReadLinkState(pContext);
             if (pContext->bLinkDetectSupported)
             {
                 ReadLinkState(pContext);
@@ -2135,6 +2144,9 @@ NDIS_STATUS ParaNdis_PowerOn(PARANDIS_ADAPTER *pContext)
 
     DEBUG_ENTRY(0);
     ParaNdis_DebugHistory(pContext, hopPowerOn, NULL, 1, 0, 0);
+
+    pContext->m_StateMachine.NotifyPowerOn();
+
     ParaNdis_ResetVirtIONetDevice(pContext);
     virtio_add_status(&pContext->IODevice, VIRTIO_CONFIG_S_ACKNOWLEDGE | VIRTIO_CONFIG_S_DRIVER);
     /* virtio_get_features must be called with any mask once upon device initialization:

NetKVM/Common/ndis56common.h
@@ -453,6 +453,7 @@ typedef struct _tagPARANDIS_ADAPTER
     CParaNdisCX CXPath;
     BOOLEAN bCXPathAllocated;
     BOOLEAN bCXPathCreated;
+    BOOLEAN bDeviceNeedsReset;
 
     CPUPathBundle               *pPathBundles;
     UINT                        nPathBundles;

NetKVM/wlh/ParaNdis6-Driver.cpp
@@ -496,8 +496,10 @@ Required NDIS handler: happens unually each 2 second
 ***********************************************************/
 static BOOLEAN ParaNdis6_CheckForHang(NDIS_HANDLE miniportAdapterContext)
 {
-    UNREFERENCED_PARAMETER(miniportAdapterContext);
-    return FALSE;
+    PARANDIS_ADAPTER *pContext = (PARANDIS_ADAPTER *)miniportAdapterContext;
+    BOOLEAN ret = pContext->bDeviceNeedsReset;
+    pContext->bDeviceNeedsReset = FALSE;
+    return ret;
 }
 
 /**********************************************************

commit 3b68ceada27131d572f88636e56956d506fa41d4

netkvm: fix mistake in observer logic
Commit 1741d661d8f63d2d89a15725773c8eb55371f90b (Sept 2018)
introduced 'need reset' flow and the plan was to enable
hardware access when power-on flow starts. However the code
enables hardware access on any message except of two.
Fixing the mistake.

Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

NetKVM/Common/ParaNdis-AbstractPath.h
@@ -120,7 +120,7 @@ public:
         {
             m_VirtQueue.DoNotTouchHardware();
         }
-        else if (PoweredOn)
+        else if (message == PoweredOn)
         {
             m_VirtQueue.AllowTouchHardware();
         }